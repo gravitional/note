f6beef3f8bae9af266dbb5af29a2abb0bffac4ee
graviton <albertains@163.com>
2022-10-28 14:27:44 +0800

    add singleton comment

--- cppLibs/singleton/singleton.cpp
+++ cppLibs/singleton/singleton.cpp
@@ -1,28 +1,30 @@
 #include "singleton.h"
 #include <iostream>
-#include<initializer_list>
+#include <initializer_list>
 
 class A : public Singleton<A>
 {
 public:
-	A() = default;
-	~A() = default;
-	int get()
-	{
-		return hold;
-	}
+    A() = default;
+    //子类需要提供此构造函数,用于 call of GetInstance()
+    A(token){};
+    ~A() = default;
+    int get()
+    {
+        return hold;
+    }
 
 private:
-	int hold = 1;
+    int hold = 1;
 };
 // 定义全局函数, 用于获取单例
-inline A* getTe()
+inline A *getTe()
 {
-	return &A::GetInstance();
+    return &A::GetInstance();
 }
 
-int main(int* argc, char* argv[])
+int main(int *argc, char *argv[])
 {
-	auto a = getTe()->get();
-	std::cout << a << std::endl;
+    auto a = getTe()->get();
+    std::cout << a << std::endl;
 }
--- cppLibs/singleton/singleton.h
+++ cppLibs/singleton/singleton.h
@@ -1,34 +1,39 @@
-/*****************************************************************//**
+/*****************************************************************
  * @file   test.h
  * @brief  可继承单例类, CRTP, 模板参数为子类
- *
  * @author yd
  * @note  跨库使用的子类必需导出，否则无法保证实例全局唯一
  * @date   October 2022
  *********************************************************************/
-
 #pragma once
-template<typename T>
-class Singleton {
+template <typename T>
+class Singleton
+{
 protected:
-	Singleton() = default;
-	virtual ~Singleton() = default;
-	/**
-	 * @brief 代理类，用于禁止用户调用派生类构造函数，保证对象唯一性.
-	 */
-	struct token {};
+    Singleton() = default;
+    virtual ~Singleton() = default;
+    /**
+     * @brief 代理类，用于禁止用户调用派生类构造函数，保证对象唯一性.
+     */
+    struct token
+    {
+    };
+
 private:
-	/**
-	 * @brief 禁止拷贝构造与拷贝赋值.
-	 */
-	Singleton(const Singleton&) = delete;
-	Singleton& operator=(const Singleton&) = delete;
+    /**
+     * @brief 禁止拷贝构造与拷贝赋值.
+     */
+    Singleton(const Singleton &) = delete;
+    Singleton &operator=(const Singleton &) = delete;
+
 public:
-	/**
-	 * @brief 取得唯一实例.
-	 */
-	static T& GetInstance() {
-		static T instance{ token() };
-		return instance;
-	}
+    /**
+     * @brief 取得唯一实例, 派生类应该提供 T(token arg)构造函数
+     */
+    static T &GetInstance()
+    {
+        //调用 T(token) 构造函数
+        static T instance{token()};
+        return instance;
+    }
 };
