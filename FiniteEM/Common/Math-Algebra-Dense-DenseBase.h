45e91dc6141e5cb633b509a816695b57694a76b4
graviton <albertains@163.com>
2023-04-11 19:27:23 +0800

    fem;c++

--- FiniteEM/Common/Math-Algebra-Dense-DenseBase.h
+++ FiniteEM/Common/Math-Algebra-Dense-DenseBase.h
@@ -15,6 +15,47 @@ template <typename T>
 class MATH_API DenseBase
 {
 public:
+    // 默认构造函数，不建议使用
+    DenseBase();
+    // 零初始化构造函数
+    DenseBase(int row, int cols);
+    // 数据指针初始化函数，此时不拥有数据所有权，修改后亦同时修改外部数据，无法更改尺寸
+    DenseBase(const T *data, int row, int cols) : _variableLength(true)
+    {
+        SetSize(rows, cols, true); // 会对数据进行置零
+    }
+
+    DenseBase(T *data, int row, int cols);
+
+    // 拷贝构造函数
+    DenseBase(const DenseBase &other);
+    DenseBase(DenseBase &&other);
+
+protected:
+    // 复制对象
+    void Copy(const DenseBase &other);
+    void Copy(DenseBase &&other);
+    // 设置行列数
+    void SetSize(int rows, int cols, bool initZero = false)
+    {
+        assert(rows >= 0 && cols >= 0 && _variableLength);
+        _rows = rows;
+        _cols = cols;
+        _data.resize(rows * cols);
+        _v = _data.data();
+        if (initZero)
+            SetZero();
+    }
+
+    void SetZero()
+    {
+        if (Size() > 0)
+        {
+            memset(Data(), 0, sizeof(T) * Size());
+        }
+    }
+
+public:
     // 取元素, 元素按照列优先存储
     inline T &operator()(int row, int col)
     {
