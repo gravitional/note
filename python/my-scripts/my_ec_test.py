99b463097b69acc9dc06f13a8fa37590943355ba
graviton <albertains@163.com>
2023-12-22 19:06:10 +0800

    python my script

--- python/my-scripts/my_ec_test.py
+++ python/my-scripts/my_ec_test.py
@@ -1,40 +1,85 @@
 import subprocess as proc
 from pathlib import Path
+from time import strftime, localtime, perf_counter
 
 solver = r'C:/Solver/bin/Release/solver.exe'
-solver_args = r"-tdep8"
+solver_args = r"-fp8"
 path_lst = [
-    'case4b',  # post convert fail
-    'case6',  # post convert fail
-    'case4c',  # post convert fail
+    # 1127x474mm; tet10; dyna curr; 20s/10s;
+    'anodization/anodization/Solving/SolvingDomain',
+    # 41378x17396mm; tet10; static curr;
+    'case ship/ship/Solving/SolvingDomain',
+    # 37227x15651mm; tet10; dyna curr; time: 20s/10s
+    'cathodic_protection/cathodic_protection/Solving/SolvingDomain',
 
-    'case2',
-    'case3',
+    # 60x25mm; tet10; static curr;
+    'plating/PCB/PCB1/PCB Cu plating static/Solving/SolvingDomain',
+    # 109x46mm; tet10; static curr;
+    'plating/PCB/PCB1/PCB Cu plating-shade/Solving/SolvingDomain',
 
-    'case4-2d',
-    'case4-3d',
+    # 20x8mm; tet10; static curr;
+    'plating/PCB/PCB2/PCB2 Cu plating static/Solving/SolvingDomain',
+    # 48x20mm; tet10; dyna curr; 180s/60s
+    'plating/PCB/PCB3/PCB3 Cu plating static/Solving/SolvingDomain',
+    # 1770x707mm; tet10; static curr
+    'plating/plating/Plating 3D APP/Solving/SolvingDomain',
+    # 1225x725mm; tet10; dyna curr 12000s/4000s
+    'plating/XXX/xxxx/Solving/SolvingDomain',
 
-    'case5',
-    'case5b',
-
-    'case7',
-    'case8',
-    'case9',
-    'case10',
+    # 54x23mm; tri6; dyna curr; 5s/1s
+    'primary cell 2D/Primary-Cell-2D-1011/Primary Cell-2D/Solving/SolvingDomain',
+    # 122x51mm; tet10; dyna curr; 5s/1s
+    'primary cell 2D/Primary-Cell-3D/Primary-Cell-3D/Solving/SolvingDomain'
 ]
 
 cwd = Path(__file__).parent  # 案例目录, 脚本放在案例仓库目录
-path_lst_psx = [(cwd/x).as_posix() for x in path_lst]
+path_lst_psx = [(cwd / x).as_posix() for x in path_lst]
+time_stamp = strftime('%Y-%d-%H-%M-%S', localtime())
+logfile = f'ec_test_log_{time_stamp}.log'
+logfile = (cwd / logfile).as_posix()
+_total_header = '=========== total time ===========\n'
 
 
 def run_solver():
-    for p in path_lst_psx:
-        print(f'>> Run solver on {p} ', 'status: ')
-        proc.run([solver, solver_args, p], check=True)
+    with open(logfile, 'w+') as flog:
+        t_tot1 = perf_counter()
+
+        for p in path_lst_psx:
+            print(f'>> Run solver on {p} ', 'status: ')
+            # run solver on path
+            flog.write(f"Run case: {p}\n")
+            flog.flush()
+
+            t_sub1 = perf_counter()
+            proc.run([solver, solver_args, p], check=True)
+            t_sub2 = perf_counter()
+
+            log_prt_time(flog, t_sub1, t_sub2)
+            flog.flush()
+
+        t_tot2 = perf_counter()
+
+        print(_total_header)
+        flog.write(_total_header)
+
+        log_prt_time(flog, t_tot1, t_tot2)
+        print("----------------------\n")
+        flog.flush()
 
 
 def copy_solu():
     pass
+
+
+# log and print time
+def log_prt_time(flog, t1: float, t2: float):
+    time_str = []
+    time_str.append(f'程序运行时间:{(t2-t1)/60.0:>-#.10G} Min')
+    time_str.append(f'程序运行时间:{(t2-t1):>-#.10G} Sec')
+    for t_str in time_str:
+        print(t_str)
+        flog.write(t_str + '\n')
+    flog.write('\n')
 
 
 if __name__ == '__main__':
--- python/my-scripts/my_update_code.py
+++ python/my-scripts/my_update_code.py
@@ -4,7 +4,7 @@ import pathlib as plb
 # ----------------- exe 路径
 # 求解器路径
 solver = 'C:/solver'
-solver_build = 'C:/solver/src/build/'
+solver_build = 'C:/solver/build/'
 # git 路径
 cmd2 = 'C:/Program Files/Git/bin/git.exe'
 # cmake 路径
@@ -14,16 +14,16 @@ cmd4 = 'C:/Program Files/CMake/bin/cmake.exe'
 arg2 = ['-C', solver, 'pull']
 cmd3 = cmd2
 # 无需担心 命令行转义, 下面使用 shell=False
-arg3 = ['-C', solver, 'submodule', 'foreach', '--recursive',
-        'echo "========================================";git pull || true']
+arg3 = [
+    '-C', solver, 'submodule', 'foreach', '--recursive',
+    'echo "========================================";git pull || true'
+]
 
-
-arg4 = ['-DRELEASE_WITH_DEBUG_INFO=OFF',
-        '-S', solver, '-B', solver_build]
+arg4 = ['-DRELEASE_WITH_DEBUG_INFO=OFF', '-S', solver, '-B', solver_build]
 
 
 def init():
-    print('================================= pull from repo solver repo\n')
+    print('================================= pull from repo: solver\n')
     p = subp.Popen([cmd2, *arg2], shell=False)
     p.communicate()
 
@@ -31,10 +31,11 @@ def init():
     p = subp.Popen([cmd3, *arg3], shell=False)
     p.communicate()
 
-    print('\n================================= Running Cmake\n')
+    print('\n================================= running CMake\n')
     p = subp.Popen([cmd4, *arg4], shell=False)
     p.communicate()
 
 
 if __name__ == '__main__':
     init()
+    input('输入任意字符, 并按Enter结束:')
