35f1e66a87dc8905fa688f3bb0360e2f841583b9
graviton <albertains@163.com>
2023-12-29 19:46:14 +0800

    echem manual; py srcipt

--- python/my-scripts/my_archive_7z.py
+++ python/my-scripts/my_archive_7z.py
@@ -1,38 +1,46 @@
 import subprocess as subp
 import time
-import pathlib as plb
+import pathlib as ptl
+import os
 
 _t1 = time.perf_counter()
-
-cwd = plb.Path(__file__).parent
-name_lst = [
+cwd = ptl.Path(__file__).parent
+path_lst = [
+    'case4b',  # post convert fail
+    'case6',  # post convert fail
+    'case4c',  # post convert fail
     'case2',
     'case3',
-
     'case4-2d',
     'case4-3d',
-    'case4b',
-    'case4c',
-
+    'case4-中文路径-case4',
     'case5',
     'case5b',
-    'case6',
-
     'case7',
     'case8',
     'case9',
     'case10',
+    'case11',
+    'case12',
+    'case13',
+    'case14-timer'  # 分段步长
 ]
-path_lst = [cwd/plb.Path(x) for x in name_lst]
+
+path_lst = [cwd / ptl.Path(x) for x in path_lst]
 
 out_path = 'Runable.7z'
-out_path = cwd/plb.Path(out_path)
+out_path = cwd / ptl.Path(out_path)
+if out_path.exists():
+    print('<<<<< remove old archive')
+    os.remove(out_path)
+    print('<<<<< remove done')
 
 exe_7z = 'C:/Program Files/7-Zip/7z.exe'
 cmd_7z = 'a'
 # 列表必须有 逗号, 不然会解析成 字符串拼接
 exclude = [
     r"-xr!result",
+    r"-xr!cache",
     r"-xr!__pycache__",
     r"-xr!result_backup",
     r"-xr!result - 副本",
@@ -41,10 +49,13 @@ exclude = [
 
 
 def my_7zip():
+    print('<<<<< start process 7z a:')
     _t1 = time.perf_counter()
-    subp.run([exe_7z, cmd_7z, out_path, *exclude, *path_lst], check=True)
+    # subp.run([exe_7z, cmd_7z, out_path, *exclude, *path_lst], check=True)
+    p = subp.Popen([exe_7z, cmd_7z, out_path, *exclude, *path_lst], shell=False)
+    p.communicate()
     _t2 = time.perf_counter()
-    _ttt = (_t2-_t1)*1
+    _ttt = (_t2 - _t1) * 1
     print(f'zip time cost: {_ttt} seconds')
 
 
