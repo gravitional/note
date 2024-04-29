063f8c87042d70927d7ed4951505a9b7b598d389
graviton <albertains@163.com>
2024-04-29 19:06:47 +0800

    call stack MF

--- cpp/src/c9-1-std-vector-bool.md
+++ cpp/src/c9-1-std-vector-bool.md
@@ -38,8 +38,8 @@ int main()
 }
 ```
 
-搜索 C++ reference1, 在里面我们可以看到 `vector<bool>` 原来是vector模板类的一个偏特化,
-而并不是由 vector 直接实例化得到的. C++ reference给我们的解释是,
+搜索 C++ reference1, 在里面我们可以看到 `vector<bool>` 原来是 `vector` 模板类的一个偏特化,
+而并不是由 vector 直接实例化得到的. C++ reference 给我们的解释是,
 为了便于把 `bool` 优化成比特存储, 而不需要一个字节来存储.
 为了探寻 `range_based_for` 失败的原因, 我们查阅C++标准2中 `vector<bool>` 的定义(略去了不必要的代码):
 
@@ -75,12 +75,13 @@ namespace std
 }
 ```
 
-结果非常的amazing啊!我们对 `vector<bool>` 的每个元素进行访问的时候,
+结果非常的amazing啊! 我们对 `vector<bool>` 的每个元素进行访问的时候,
 实际上访问的都是 `vector<bool>::reference` 这个类, 而不是其成员本身.
 毕竟如果编译器想要将其每个元素优化成二进制位的话,
-C++语言由于不存在直接访问二进制位的操作, 就无法直接访问其每个元素, 所以统一为访问这个类来对每个元素进行引用.
-了解了这些, 我们想解答编译出错的事情, 还需要知道range_based_for的实现.
-C++标准2中如此规定range_based_for:
+C++语言由于不存在直接访问二进制位的操作, 就无法直接访问其每个元素,
+所以统一为访问这个类来对每个元素进行引用.
+了解了这些, 我们想解答编译出错的事情, 还需要知道 `range_based_for` 的实现.
+C++标准2中如此规定 `range_based_for`:
 
 ```cpp
 for( for-range-declaration : for-range-initializer ) statement
@@ -101,23 +102,24 @@ for( for-range-declaration : for-range-initializer ) statement
 ```cpp
 vector<bool> v;
 {
-    auto &&__range = v;             // auto被推导为vector<bool>&, 因此__range为auto&&&, 即auto&
-    auto __begin = __range.begin(); // auto被推导为vector<bool>::ierator
+    auto &&__range = v;             // auto 被推导为vector<bool>&, 因此__range为 auto&&&, 即auto&
+    auto __begin = __range.begin(); // auto 被推导为 vector<bool>::ierator
     auto __end = __range.end();     // 同上
     for (; __begin != __end; ++__begin)
     {
-        auto &velem = __begin.operator*(); // error! velem被推导为vector<bool>::reference&
+        auto &velem = __begin.operator*(); // error! velem 被推导为 vector<bool>::reference&
         statement
     }
 }
 ```
 
-显然标记了//error!的那一行就产生了错误!因为 `__begin.operator*()` 是一个函数调用,
+显然标记了 `//error!` 的那一行就产生了错误!
+因为 `__begin.operator*()` 是一个函数调用,
 而函数的返回值是一个右值, 即 `vector<bool>::reference` 的临时对象,
 而我们却给它赋值给了左值引用 `vector<bool>::reference&`, 因此会报出编译错误.
 
-那么, 我们如果要修改v的元素该怎么办呢?这是一个令人发指的操作: 
-注意到我们的 `vector<bool>::reference是对vector<bool>` 的一个元素的引用(可以理解为指针),
+那么, 我们如果要修改 `v` 的元素该怎么办呢? 这是一个令人发指的操作:
+注意到我们的 `vector<bool>::reference` 是对 `vector<bool>` 的一个元素的引用(可以理解为指针),
 那么我们只需要值传递 `vector<bool>::reference` 即可, 不需要进行引用传递!即代码如下:
 
 ```cpp
@@ -129,7 +131,7 @@ for (auto velem : v)
 ::std::cout << (int)v[5] << ::std::endl;
 ```
 
-输出的结果果然是1!这个操作与其他类型的vector完全不同.
+输出的结果果然是`1`! 这个操作与其他类型的 `vector` 完全不同.
 因为我们知道, 把 `vector<bool>` 换成 `vector<int>` 的话:
 
 ```cpp
@@ -141,7 +143,8 @@ for (auto velem : v)
 ::std::cout << (int)v[5] << ::std::endl;
 ```
 
-它的输出结果显然仍然是0, 因为值传递不1能改变v内元素的值, 而 `vector<bool>` 与我们的习惯完全不同!这是需要特别注意的.
+它的输出结果显然仍然是`0`, 因为值传递不`1`能改变`v`内元素的值,
+而 `vector<bool>` 与我们的习惯完全不同! 这是需要特别注意的.
 
 我们回到最开始的问题, 我起初报错的代码就可以改成:
 
@@ -150,9 +153,10 @@ for (auto velem : v)
 using ::std::vector;
 int main()
 {
+    using std::vector;
     vector<vector<bool>> v(10, vector<bool>(10, false));
     for (auto &vx : v)
-        for (auto velem : vx) // 此处为auto, 并非auto&!
+        for (auto velem : vx) // 此处为auto, 并非 auto&!
         {
             velem = true;
         }
@@ -160,7 +164,8 @@ int main()
 }
 ```
 
-这就是 `vector<bool>` 与普通的vector的不同之处.
-因此, C++ reference中也警告我们, 我们无法使用 `无特化的bool类型`.
-如果这种特化的bool类型不能满足我们的需求,
-我们只能用 `char`, `unsigned char` 或其他的封装类型来代替, 或者使用其他的容器如deque来代替.
+这就是 `vector<bool>` 与普通的 `vector` 的不同之处.
+因此, C++ reference 中也警告我们, 我们无法使用 `无特化的bool类型`.
+如果这种特化的 `bool` 类型不能满足我们的需求,
+我们只能用 `char`, `unsigned char` 或其他的封装类型来代替,
+或者使用其他的容器如 `deque` 来代替.
---
+++ simdr/src/sv-stack/callStack-MF-Static.h
@@ -0,0 +1,144 @@
+// 求解器调用栈, 静磁场
+//solver/main.cpp
+// vt: virtual
+// nv: not virtual
+
+main(int argc, char* argv[]);
+    common::Control().Execute(argc, argv); // 总流程控制类, 管理 Impl 资源, /Common/Control/Control.cpp
+        args(); //解析命令行参数
+        _pIml->Initialize(argc, argv); // 初始化 Blas, mpi, cuda, thread, hypre, petsc, profiler(分析器)
+        anlsCtrl()->TestLicense(); //检查许可证
+        _pIml->SetWorkingPath(); //设置工作路径
+        _pIml->PrintHeader(); //打印日志头, 成刷版本, 进程数目等.
+        _pIml->CreateModel(); //创建模型
+            reader=controlReader(); // read control and solver, 从 control.json
+            anlsCtrl()->TestLicense(solver); //
+            lib()->Load(Str::ToLower(solver)); // load solver dll
+
+            memTracker()->SnapShot("CreateModel"); //分析内存
+            unique_ptr<ModelCreator> creator(ModelCreator::New(solver));// 创建 solver model
+            creator->Create(); // 创建网格和模型, /Common/Model/ModelCreator.cpp
+                SetFieldInfo(); //设置场量的 量纲 单位
+                CreateMesh(); // 创建网格
+                CreateModel(); //创建模型 /FiniteElement/Model/FeModelCreator.cpp
+                    CreateFunction(); //nv; 自定义函数
+                    CreateCoordSys(); //nv; 自定义坐标系
+                    CreateMonitor();  //nv;创建监视
+                    CreateNode(); // nv; 创建节点
+                        CompNode* node=NewNode(); //节点工厂方法, 调用具体实现
+                    CreateElement(data);  //nv;创建单元
+                    CreateMaterial(data); //nv; 创建材料
+                    CreateMisc(data); //vt; 创建各场的特有数据, /Current/Model/ModelCreatorCF.cpp
+                        modelMF()->Initialize(); // eddy effect; ele on ConsSurf; node on ConsSurf; node edge sign;
+                        CreateInfo(data); // 创建全局参数
+                            data.ReadValue("Thickness2D"...) //读取厚度
+                            data.ReadValue("Sector"...) //读取模型分数
+                        CreateNetlist(data); // 创建 net list
+                        CreateEddy(data); // 创建 eddy 效应;
+                            eddy->Create(data);
+                                SetEddy(theID); // 设置 Eddy 效应
+                                    modelMF()->SetEddyEffect(id); // 设置 eleID eddyEffect 为 true
+                        CreateMotion(data); // 创建 运动;
+                        CreateConnectedCoils(data);// 创建连接线圈
+                        CreateConstraint(data) // 创建约束, 即边界条件,
+                        // Vector, Open, IBC, Periodic, NonConform2D, NonConform3D
+                    CreateInitialField(data); //vt; 创建初始场
+                    CreateAnalysis(); //vt; 创建分析, /FiniteElement/Model/FeModelCreator.cpp
+                        //迭代分析类型
+                        analysis->Read(); // 动态多态, 转向 AnalysisFe::Read()
+                            auto reader=controlReader(); // control.json 单例
+                            auto job =NewJob(); //任务工厂方法
+                            _jobID=model()->AddComponentGenID(job); // 产生 _jobID, 即 工况号, AnalysisBase 成员
+                            _name=reader->GetCurrentNode(); // 获取有限元分析名称
+                            reader->SetIDByName(_name,_jobID);// 设置分析名称(_name) 对应的 ID(jobID), 存入unordered_map<string, int> _nameMap, 不同 reader 共享
+                            job->Create(*reader); //virtual, 创建 new job
+                                NonlinearPara.Create(data); //非线性迭代; /Current/Analysis/LoadJobCF.cpp
+                                DynamicPara.Create(data); // 时间步迭代; 欧拉法/梯形法; 固定步长/自适应步长
+                                CreateCouple(); // 耦合
+                                CreateLoad(); // 创建载荷
+                                int id=model()->AddComponentGenID(_PostAnalysis); // 添加后处理分析任务
+                        anlsCtrl()->AddAnalysis(analysis) // append 分析到分析队列末尾
+                            //非续算时清空独立计时器
+                            //清空当前分析的标记 _flags
+                            //设置新的变量输出
+
+                    Initialize(); // 各个场自己的特定初始化, /Current/Model/ModelCreatorCF.cpp
+                        model()->CopyThreadsMaterial(); //复制线程私有材料对象
+                        CreateEdge(); // 创建边单元
+                        ApplyConstraint(); // 施加约束
+                            cons->ExpandCons2Ele();//施加约束到单元上
+                            eddy->EddyEffect2FEM(); // Eddy 效应
+                            coil->InitialCoils(); // 初始化 线圈
+                            mpc->BuildMstSlvDofConnection();// 建立主从约束; 多点共自由度
+                            edge->Restrain(0); // MPI edge restrain
+                        ArrangeEquationNo(); // 数据自由度排序
+                        MPIRenumber(); // MPI 重新排序
+                        CreateElementList(); // 生成单元列表
+                        globalInfo()->SetDouble("Penalty",1e60)// 罚系数确定
+                        ApplyExcitation(); //                        
+                        WriteGeoInfo(); //写出网格信息
+
+            memTracker()->SnapShot("CreateModel"); //分析内存,
+//----------------------------------------------------------------------------------------------------- to update
+        _pIml->AnalyzeModel(); //运行模型分析, /Common/Control/Control.cpp
+            //迭代每个分析
+            while(true){
+                auto analysis = anlsCtrl()->GetNextAnalysis();
+                    // /Common/Analysis/AnalysisControl.cpp:80; 设置新的变量输出, 获取分析 输出的场范围
+                    std::vector<std::string> fields=_curAnalysis->GetOutFields();
+                memTracker();timeTracker(); // 记录内存, 时间占用
+                analysis->Run(); // 运行每个分析. /Common/Analysis/AnalysisBase.cpp
+                    Analyze(); // 虚函数, /Current/Analysis/AnalysisDynaCF.cpp
+                        InitBeforeJobLoop(); // 电流场分析初始化
+                            ResultManipEM* p_resultHandle=resultManip(); //初始化计算结果存储指针
+                            // 初始化 p_GFinAccum, p_GRt, p_GFv_T, p_Solu_T, P_GFv_Pre
+                            RecordRestrictedEqNo(); // 记录受到约束的自由度
+                            cons->SetValue(); // 设置边界条件, 约束值
+                        // 根据设置, 运行不同分析算法,
+                        analyze_EM_Field_Fixed(); // 非线性迭代, 固定时间步长
+                            //---------------- 时间循环开始
+                            // ---- 结果导入, 续算
+                            // ---- 零时刻计算
+                            UpdateExcitationsConstraint(_timer->GetTime());//更新激励和约束
+                            SolveOneTimeStep(_jobID); //
+                                CalculateTotalForce(); //计算total force, 存入 p_GRt
+                                    ele->CalcuElementFin(internalFv); //单元对内力的贡献
+                                    cons->AssembleFin(p_GFinAccum); //计算约束对应的内力
+                            AssembleGlobleK(Sparse::GetMatrix<double>("K")); //组装总体刚度矩阵
+                                ele->CalcuElementMatrixKe(Ke);//组装单元上的局域刚度矩阵, 迭代所有单元, 包括体单元, 面单元, 线单元.
+                                cons->AssembleKe(mat); //组装约束对应的刚度矩阵
+                                    ElePt->CalcEleMatrixKeOpen(ke);//开放边界的贡献
+                                    ElePt->CalcEleMatrixKeShield(ke);//介电屏蔽的贡献
+                            CallSolver_T(K,p_GRt.data(),p_Solu_T.data());//求解方程
+                            LineSearch();//回溯线搜索, 广义牛顿法
+                            p_resultHadle->CopySoluData(STP_SEARCH,_jobID,1);//复制解到 线搜索存储空间
+                            p_resultHadle->AccumSoluData(STEPBEG_ACCUM,STP_SEARCH); // 将结果累加
+                            bool converged= false;//判断解是否收敛
+                            PostProcessStepFiledData(); // 后处理, 当前时间步场数据
+                                AlgPostAnalysisEF analysis; // 新建Post分析对象
+                                analysis.Run(); // 运行后处理分析, 电磁力, 力矩, 电容
+                                    PostProcessElementData(jobID);//计算场量如 V,E,D,Energy 则高斯点和节点的值
+                                    PostProcessElementNodeForce(jobID);//计算节点上受的电磁力
+                                    _PostAnalysis->Analysis();// 依次运行特定的后处理任务
+                                    auto io=common::resultWriter()->GetID(); // 输出结果到 h5
+                                    Print2TxtFile();//输出结果到 txt 文件
+                                FieldWriterEF* writer=objects()->GetUniPtr<FieldWriterEF>();
+                                writer->Write(_job, "Static"); // 写入场量到 result.h5
+                                    WriteField(); // 写入到 hdf5
+                                    SetTestFieldData(); // 写入到 ensight
+
+                        analyze_Coupled_Field(); // 耦合场分析
+                        analyze_EM_Field_Adapt(); // 非线性迭代, 自适应时间步长
+
+                memTracker();timeTracker(); // 记录内存, 时间占用
+            }
+        _pIml->Finalize(); // 计算收尾, /Common/Control/Control.cpp
+            ThreadUtil::WaitAll(); // async
+            timeTracker();memTracker(); //profiler
+            objects()->Clear(); // objects
+            Pyutil::Finalize(); //python
+            PetscUtil::Finalize(); //petsc
+            HypreUtil::Finalize(); //hypre
+            AmgxUtil::Finalize(); //amgx
+            MPIUtil::Finalize(); //mpi
+            CUDAUtil::Finalize(); //gpu
