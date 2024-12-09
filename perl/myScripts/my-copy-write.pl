de09dd6701dd641f0e8abfe2e1c756bdc4f43fe0
gravtion <albertains@163.com>
2024-12-09 18:44:09 +0800

    perl copy scripts;

--- perl/myScripts/my-copy-write.pl
+++ perl/myScripts/my-copy-write.pl
@@ -2,8 +2,10 @@
 use 5.032;
 use strict;
 use warnings;
-use Cwd qw(cwd abs_path);
+use Cwd        qw(cwd abs_path);
+use File::Path qw(make_path);
 use File::HomeDir;
+use File::Spec;
 use File::Copy::Recursive qw(rcopy rmove rcopy_glob rmove_glob);
 
 # https://perl-book.junmajinlong.com/ch10/3_file_copy_move.html
@@ -11,11 +13,14 @@ use File::Copy::Recursive qw(rcopy rmove rcopy_glob rmove_glob);
 #---- 支持通配的拷贝和移动，采用File::Glob::bsd_glob()的通配规则
 # perltidy -nst -b -bext='/' -gnu -pbp -i=2 -ci=2 -ce -nbl -bt=2 -sbt=2 -pt=2 -nsfs -nsak=s -dws
 my $home   = File::HomeDir->my_home;
-my $source = abs_path(qq[c:/sim-solver/Release]);
-my $target = abs_path(qq[$home/test/dll-test]);
+my $source = File::Spec->catfile('c:',    'sim-solver', 'Release');
+my $target = File::Spec->catfile("$home", 'test',       'dll-test');
 say($source);
 say($target);
-rcopy_glob("${source}*.{dll,exe}", $target) or die $!;
+
+# 创建 target 目录
+make_path "$target";
+rcopy_glob("${source}/*.{dll,exe}", "$target") or die $!;
 if ($@) {
   warn "Caught exception: $@";
 }
