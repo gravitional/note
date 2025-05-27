cdd646c5d51d5b070a2ba1597d36c8b9d73e6227
gravtion <albertains@163.com>
2025-05-27 20:13:07 +0800

    add vscode plugins download

---
+++ editor/src/vscode_vsix_plugins.py
@@ -0,0 +1,23 @@
+#******************* CMake Language Support
+publisher = 'josetr'
+plugin_name = 'cmake-language-support-vscode'
+version = '0.0.9'
+#
+url = f'https://marketplace.visualstudio.com/_apis/public/gallery/publishers/{publisher}/vsextensions/{plugin_name}/{version}/vspackage'
+print(f'\n{plugin_name}: {url}')
+
+#******************* CMake Format
+publisher = 'cheshirekow'
+plugin_name = 'cmake-format'
+version = '0.6.11'
+#
+url = f'https://marketplace.visualstudio.com/_apis/public/gallery/publishers/{publisher}/vsextensions/{plugin_name}/{version}/vspackage'
+print(f'\n{plugin_name}: {url}')
+
+#******************* CMake Tools
+publisher = 'ms-vscode'
+plugin_name = 'cmake-tools'
+version = '1.21.28'
+#
+url = f'https://marketplace.visualstudio.com/_apis/public/gallery/publishers/{publisher}/vsextensions/{plugin_name}/{version}/vspackage'
+print(f'\n{plugin_name}: {url}')
